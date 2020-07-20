ec5ddd6be6a3f7002ff469ba1c802105ec54f999
moseskamira <moses.african@gmail.com>
2020-07-20 19:01:15 +0300

    More Swagger fix

--- src/main/java/com/online/shopping/service/ProductService.java
+++ src/main/java/com/online/shopping/service/ProductService.java
@@ -13,74 +13,74 @@ import com.online.shopping.models.ProductCategory;
 @Service
 public class ProductService {
 	@Autowired
-	ProductDAO productDao;
+	ProductDAO songDao;
 	
 	@Autowired
-	ProductCategoryDAO productCategoryDao;
+	ProductCategoryDAO albumDao;
 	
-	public Product saveProduct(Long prodCatId, Product product) {
-		ProductCategory prodCat = productCategoryDao.getOne(prodCatId);
-		Product myProduct = new Product();
-		myProduct.setProdCat(prodCat);
-		myProduct.setProdName(product.getProdName());
-		myProduct.setProdDescrip(product.getProdDescrip());
-		myProduct.setProdImageUrl(product.getProdImageUrl());
-		myProduct.setAudioUrl(product.getAudioUrl());
-		myProduct.setStudioName(product.getStudioName());
-		myProduct.setProducerName(product.getProducerName());
-		myProduct.setUnitPrice(product.getUnitPrice());
-		myProduct.setProdLike(product.isProdLike());
-		Product savedProduct = productDao.saveAndFlush(myProduct);
-		return savedProduct;
+	public Product saveSong(Long albumId, Product song) {
+		ProductCategory album = albumDao.getOne(albumId);
+		Product mySong = new Product();
+		mySong.setProdCat(album);
+		mySong.setProdName(song.getProdName());
+		mySong.setProdDescrip(song.getProdDescrip());
+		mySong.setProdImageUrl(song.getProdImageUrl());
+		mySong.setAudioUrl(song.getAudioUrl());
+		mySong.setStudioName(song.getStudioName());
+		mySong.setProducerName(song.getProducerName());
+		mySong.setUnitPrice(song.getUnitPrice());
+		mySong.setProdLike(song.isProdLike());
+		Product savedSong = songDao.saveAndFlush(mySong);
+		return savedSong;
 	}
 	
-	public List<Product> getAllProducts() {
-		List<Product> productList = productDao.findAll();
-		return productList;
+	public List<Product> getAllSongs() {
+		List<Product> songList = songDao.findAll();
+		return songList;
 	}
 	
-	public List<Product> getProductsPerCategory(Long prodCatId) {
-		ProductCategory prodCat = productCategoryDao.getOne(prodCatId);
-		List<Product> catProdList = prodCat.getProducts();
-		return catProdList;
+	public List<Product> getSongsPerAlbum(Long albumId) {
+		ProductCategory album = albumDao.getOne(albumId);
+		List<Product> songsList = album.getProducts();
+		return songsList;
 	}
 	
-	public Product getSingleProduct(Long prodCatId, Long prodId) {
-		ProductCategory prodCat = productCategoryDao.getOne(prodCatId);
-		List<Product> catProdList = prodCat.getProducts();
-		Product myProduct = null;
-		if(!catProdList.isEmpty()) {
-			for(Product product : catProdList) {
-				if(product.getProdId()==prodId) {
-					myProduct=product;
+	public Product getSingleSong(Long albumId, Long songId) {
+		ProductCategory album = albumDao.getOne(albumId);
+		List<Product> songsList = album.getProducts();
+		Product mySong = null;
+		if(!songsList.isEmpty()) {
+			for(Product song : songsList) {
+				if(song.getProdId()==songId) {
+					mySong=song;
 				}
 			}
 		}
-		return myProduct;
+		return mySong;
 	}
 	
-	public String deleteProduct(Long prodCatId, Long prodId) {
-		Product productToBeDeleted = productDao.getOne(prodId);
-		productDao.delete(productToBeDeleted);
-		return "Successfully Deleted Product";
+	public String deleteSong(Long albumId, Long songId) {
+		Product songToBeDeleted = songDao.getOne(songId);
+		songDao.delete(songToBeDeleted);
+		return "Successfully Deleted Song";
 	}
 	
-	public Product updateProduct(Long catId, Long prodId, Product product) {
-		ProductCategory category = productCategoryDao.getOne(catId);
-		List<Product> myProdList = category.getProducts();
-		Product updatedProduct = null;
-		if(!myProdList.isEmpty()) {
-			for(Product prodToUpdate : myProdList) {
-				if(prodToUpdate.getProdId()==prodId) {
-					prodToUpdate.setProdName(product.getProdName());
-					prodToUpdate.setProdDescrip(product.getProdDescrip());
-					prodToUpdate.setProdImageUrl(product.getProdImageUrl());
-					updatedProduct = productDao.saveAndFlush(prodToUpdate);
+	public Product updateSong(Long albumId, Long songId, Product song) {
+		ProductCategory album = albumDao.getOne(albumId);
+		List<Product> mySongsList = album.getProducts();
+		Product updatedSong = null;
+		if(!mySongsList.isEmpty()) {
+			for(Product songToUpdate : mySongsList) {
+				if(songToUpdate.getProdId()==songId) {
+					songToUpdate.setProdName(song.getProdName());
+					songToUpdate.setProdDescrip(song.getProdDescrip());
+					songToUpdate.setProdImageUrl(song.getProdImageUrl());
+					updatedSong = songDao.saveAndFlush(songToUpdate);
 				}
 			}
 		}
 		
-		return updatedProduct;
+		return updatedSong;
 		
 	}
 }
